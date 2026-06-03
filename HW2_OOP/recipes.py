cc58929da8a06a53fd02433e25fd363b74ad4af5
Дорохова Дарья <dadorokhova@edu.hse.ru>
2026-06-03 23:15:01 +0300

    Добавила класс DietaryRecipe

--- HW2_OOP/recipes.py
+++ HW2_OOP/recipes.py
@@ -141,3 +141,22 @@ class ShoppingList:
             )
             new_shopping_list._items.append((new_ingredient, recipe_title))
         return new_shopping_list
+
+
+class DietaryRecipe(Recipe):
+    def __init__(self, title, diet_type, ingredients=None):
+        super().__init__(title, ingredients)
+        self.diet_type = diet_type
+
+
+    def scale(self, ratio):
+        scaled_recipe = super().scale(ratio)
+        return DietaryRecipe(
+            self.title,
+            self.diet_type,
+            scaled_recipe.ingredients
+        )
+
+
+    def __str__(self):
+        return "[" + self.diet_type + "] " + super().__str__()
